5683e053d72d1ea94e55688801a1427e1888302c
Guillaume Leroy <pro.guillaume.leroy@gmail.com>
2023-09-20 18:34:35 +0200

    feat: add HTTP client

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,12 @@
 pub use self::clock::MockClock;
 #[cfg(feature = "clock")]
 pub use self::clock::{Clock, DefaultClock};
+#[cfg(feature = "http-client")]
+pub use self::http::{
+    DefaultHttpClient, DefaultHttpResponse, HttpClient, HttpRequest, HttpResponse,
+};
+#[cfg(all(feature = "http-client", feature = "mock"))]
+pub use self::http::{MockHttpClient, MockHttpResponse};
 #[cfg(any(feature = "mock", test))]
 pub use self::mock::Mock;
 #[cfg(all(feature = "uuid", feature = "mock"))]
@@ -31,6 +37,8 @@ pub use self::{
 mod clock;
 mod env;
 mod fs;
+#[cfg(feature = "http-client")]
+mod http;
 #[cfg(any(feature = "mock", test))]
 mod mock;
 mod sys;
