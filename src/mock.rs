c830cb842822d3dce153b89c5ff183bd4cc9e04b
Guillaume Leroy <pro.guillaume.leroy@gmail.com>
2023-09-22 16:42:59 +0200

    docs(mock): add example

--- src/mock.rs
+++ src/mock.rs
@@ -7,54 +7,7 @@ use std::sync::{
 ///
 /// **This is supported on `feature=mock` only.**
 ///
-/// # Examples
-/// ```
-/// use mockable::Mock;
-/// use mockall::automock;
-///
-/// // Never
-/// let mock: Mock<()> = Mock::never();
-/// // fist call will panic
-///
-/// // Once
-/// let mock = Mock::once(|| 42);
-/// assert_eq!(mock.call(), 42);
-///
-/// // Several
-/// let mock = Mock::with(vec![
-///     Box::new(|| 1),
-///     Box::new(|| 2),
-///     Box::new(|| 3)]
-/// );
-/// assert_eq!(mock.call(), 1);
-/// assert_eq!(mock.call(), 2);
-/// assert_eq!(mock.call(), 3);
-/// // next call will panic
-///
-/// // Always
-/// let mock = Mock::always(|idx| idx);
-/// assert_eq!(mock.call(), 0);
-/// assert_eq!(mock.call(), 1);
-/// assert_eq!(mock.call(), 2);
-/// // next call will never panic
-///
-/// // with mockall
-/// #[automock]
-/// trait MyTrait {
-///     fn foo(&self) -> &'static str;
-/// }
-///
-/// let mock = Mock::once(move || "bar");
-/// let mut mymock = MockMyTrait::new();
-/// mymock
-///     .expect_foo()
-///     .returning({
-///         let mock = mock.clone();
-///         move || mock.call()
-///     });
-/// assert_eq!(mymock.foo(), "bar");
-/// assert_eq!(mock.count(), 1);
-/// ```
+/// [Example](https://github.com/leroyguillaume/mockable/tree/main/examples/mock.rs).
 pub struct Mock<E> {
     idx: Arc<AtomicUsize>,
     kind: MockKind<E>,
